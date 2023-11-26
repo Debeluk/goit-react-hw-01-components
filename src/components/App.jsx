a6ffd1bbceedead3a2b8818c5f0e02a5d20ea3ef
Debeluk <popcorn.starinets@gmail.com>
2023-11-26 17:06:39 +0200

    2.1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Profile from './Profile/Profile';
+import Statistics from './Statistics/Statistics';
 import user from '../data/user.json';
+import data from '../data/data.json';
 
 export const App = () => {
   return (
@@ -22,6 +24,7 @@ export const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
+      <Statistics title="Upload Stats" stats={data} />
     </div>
   );
 };
