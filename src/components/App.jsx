54284dfad48fe0debfaae6655dda0c19d014d630
Debeluk <popcorn.starinets@gmail.com>
2023-11-26 21:00:34 +0200

    4.1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,6 +5,8 @@ import user from '../data/user.json';
 import data from '../data/data.json';
 import friends from '../data/friends.json'
 import FriendList from './Friends/Friends';
+import transactions from '../data/transactions.json';
+import Transactions from './Transactions/Transactions';
 
 export const App = () => {
   return (
@@ -28,6 +30,7 @@ export const App = () => {
       />
       <Statistics title="Upload Stats" stats={data} />
       <FriendList friends={friends} />
+      <Transactions items={transactions} />
     </div>
   );
 };
