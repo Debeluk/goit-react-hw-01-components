e1b9f9837e7e6207bdb686a00b072d9f04636c58
Debeluk <popcorn.starinets@gmail.com>
2023-11-26 13:29:49 +0200

    1.2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,28 @@
+import React from 'react';
+import Profile from './Profile/Profile';
+import user from '../../user.json'; 
+
 export const App = () => {
   return (
     <div
       style={{
         height: '100vh',
         display: 'flex',
+        flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
+        padding: 20,
       }}
     >
-      React homework template
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
     </div>
   );
 };
